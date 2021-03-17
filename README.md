fe0f8a3ed0fec0a9eefde84555d2acdcb7b23982
Sam M <sammontalvojr@gmail.com>
2021-03-17 14:30:07 -0500

    inital commit. Set up electron app with systeminfo package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WFH-Timer
 Creating an app that reminds the user to take breaks from work related applications
+
+## Get started
+- Install all packages with
+    npm install
+- When installation is complete, run electron with
+    npm start
---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const si=require('systeminformation');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+si.processes().then(data => console.log(data.list.find(element => element.name === 'zoom.us').state));
