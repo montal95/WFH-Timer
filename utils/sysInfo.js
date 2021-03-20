c40e770f68d59247b5033c3c44bd7c96bf58c6a0
jenesh <jenesh@codecademy.com>
2021-03-19 21:01:12 -0400

    Working: showing all zoom processes

--- utils/sysInfo.js
+++ utils/sysInfo.js
@@ -10,11 +10,19 @@ const valueObject = {
 };
 
 const zoomLogger = (data) => {
-  console.log(data.processes.list)
-  const zoomRunning = data.processes.list.find(
+  // console.log(Object.keys(data.processes.list))
+  
+  const zoomProcess = data.processes.list.filter(
     (runningProcces) => runningProcces.name === "zoom.us"
+  )
+
+  console.log(zoomProcess[0])
+
+  const zoomRunning = data.processes.list.filter(
+    (runningProcces) => runningProcces.parentPid === zoomProcess[0].pid
   );
   // nLog.Console(zoomRunning ? "active" : "inactive");
+  console.log(`Zoom apps running: `, zoomRunning)
   console.log(zoomRunning ? "active" : "inactive")
 };
 // si.get(valueObject, zoomLogger)
